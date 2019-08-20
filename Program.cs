e2dee3e126fddecc8d754bf03548a3382c0d437d
vldmrklnk <uladzimerkalenik@gmail.com>
2019-08-20 15:33:58 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticalMean
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Vvedite pervoe chislo:");
+			string chislo = Console.ReadLine();
+			double firstValue = Convert.ToDouble(chislo);
+			Console.WriteLine("Vvedite vtoroe chislo:");
+			chislo = Console.ReadLine();
+			double secondValue = Convert.ToDouble(chislo);
+			Console.WriteLine("result = "+((firstValue+secondValue)/2));
+			Console.ReadLine();
+		}
+	}
+}
